36d5fc10c638b889b6b7d8661af70feab02ed6a1
SamAtRectory <samantha.fafard@rectorywolves.org>
2025-01-23 11:53:08 -0500

    Finalized comments

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ alph = "abcdefghijklmnopqrstuvwxyz"	# Accessbile alphabet
 word = input("Give me a message. ")	# The plaintext
 key = input("Give me a key. ")	# The key
 ciphertext = ""	# Empty space for the encoded message
-j = 0
+j = 0 #index variable
 while len(ciphertext) < len(word):	# Loops the key until it matches the length of the plaintext
 	ciphertext += alph[(alph.index(word[j]) + alph.index(key[j % len(key)])) % 26]	# Creates the final message
-	j+=1
+	j+=1 #increment var
 print(ciphertext)
